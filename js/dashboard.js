b8c29686755a537121f17a4727e9e2d82b4cc620
Chaitanyaputhran <21i07.chaitanya@sjec.ac.in>
2023-08-19 23:01:20 +0530

    updated

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,5 +1,7 @@
 togglebutton.addEventListener('click',(event) =>{
     event.preventDefault();
     dashboard_sidebar.style.width='10%';
+    dashboard_content_container.style.width='90%';
+    dashboard_logo.style.fontsize= '60px';
 
 });
