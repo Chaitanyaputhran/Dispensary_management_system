61f636469f3be2d6cd15e3879114af1bee33ef7b
Chaitanyaputhran <21i07.chaitanya@sjec.ac.in>
2023-08-19 23:38:46 +0530

    updated

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,7 +1,28 @@
+var sideBarIsOpen='true';
 togglebutton.addEventListener('click',(event) =>{
     event.preventDefault();
+    if(sideBarIsOpen){
     dashboard_sidebar.style.width='10%';
+    dashboard_sidebar.style.transition='0.3s all';
     dashboard_content_container.style.width='90%';
     dashboard_logo.style.fontsize= '60px';
-
+    userimage.style.width='60px';
+    menuicons=document.getElementsByClassName('menutext');
+    for(var i=0;i<menuicons.length;i++){
+    menuicons[i].style.display='none';
+    }
+    document.getElementsByClassName('dashboard_menu_lists')[0].style.textAlign='center';
+    sideBarIsOpen=false;}else{
+        dashboard_sidebar.style.width='20%';
+    dashboard_content_container.style.width='80%';
+    dashboard_logo.style.fontsize= '80px';
+    userimage.style.width='80px';
+    menuicons=document.getElementsByClassName('menutext');
+    for(var i=0;i<menuicons.length;i++){
+    menuicons[i].style.display='inline-block';
+    }
+    document.getElementsByClassName('dashboard_menu_lists')[0].style.textAlign='left';
+    sideBarIsOpen=true;
+    }
+    
 });
