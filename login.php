8b5b930c96f2f404930deb4bbf9ec61c2a4f55f1
Chaitanyaputhran <21i07.chaitanya@sjec.ac.in>
2023-08-20 19:44:55 +0530

    added new files

--- login.php
+++ login.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dispensary Management System</title>
-    <link rel="stylesheet" href="/css/login.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo __DIR__ . '/css/login.css'; ?>">
+
 </head>
 <body>
     <div class="container">
