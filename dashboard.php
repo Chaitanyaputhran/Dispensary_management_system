0d49c8bad3e40f6b85221ad07d1f9436920702ba
Chaitanyaputhran <21i07.chaitanya@sjec.ac.in>
2023-08-22 20:14:09 +0530

    updated

--- dashboard.php
+++ dashboard.php
@@ -31,35 +31,8 @@ $user = ($_SESSION['user']);
 
         </div>
     </div>
-    <script >
-    var sideBarIsOpen='true';
-togglebutton.addEventListener('click',(event) =>{
-    event.preventDefault();
-    if(sideBarIsOpen){
-    dashboard_sidebar.style.width='10%';
-    dashboard_sidebar.style.transition='0.3s all';
-    dashboard_content_container.style.width='90%';
-    dashboard_logo.style.fontsize= '60px';
-    userimage.style.width='60px';
-    menuicons=document.getElementsByClassName('menutext');
-    for(var i=0;i<menuicons.length;i++){
-    menuicons[i].style.display='none';
-    }
-    document.getElementsByClassName('dashboard_menu_lists')[0].style.textAlign='center';
-    sideBarIsOpen=false;}else{
-        dashboard_sidebar.style.width='20%';
-    dashboard_content_container.style.width='80%';
-    dashboard_logo.style.fontsize= '80px';
-    userimage.style.width='80px';
-    menuicons=document.getElementsByClassName('menutext');
-    for(var i=0;i<menuicons.length;i++){
-    menuicons[i].style.display='inline-block';
-    }
-    document.getElementsByClassName('dashboard_menu_lists')[0].style.textAlign='left';
-    sideBarIsOpen=true;
-    }
+    <script src="js/dashboard.js" >
     
-});
 </script>
     
 </body>
