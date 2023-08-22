5a981ae2d75b644d854aedc11c61ef6669c8e6d3
Chaitanyaputhran <21i07.chaitanya@sjec.ac.in>
2023-08-22 19:50:25 +0530

    updated

--- partials/appsidebar.php
+++ partials/appsidebar.php
@@ -1,4 +1,11 @@
-<div class ="dashboard_sidebar_menus">
+<div class="dashboard_sidebar" id ="dashboard_sidebar">
+            <h3 class = "dashboard_logo" id="dashboard_logo">DMS</h3>
+            <div class ="dashboard_sidebar_user">
+                <img src="./images/user/dms.jpg" alt="user image" id="userimage"/>
+                <span ><?=$user['firstname'] .' '.$user['lastname'] ?></span>
+
+            </div>
+            <div class ="dashboard_sidebar_menus">
                 <ul class="dashboard_menu_lists" >
                     <li class="menuactive">
                         <a href="" ><i class="fa fa-dashboard "></i><span class="menutext">Dashboard</span></a>
@@ -10,3 +17,4 @@
                 </ul>
 
             </div>
+        </div>
